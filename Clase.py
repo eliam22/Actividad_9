dd418b04001e22552c818ba54513082a15810800
EliamS <49135431+eliam22@users.noreply.github.com>
2024-10-21 17:47:34 -0600

    Add files via upload

---
+++ Clase.py
@@ -0,0 +1,20 @@
+from tkinter import Image
+import numpy as np
+
+my_arr = np.arange(1_000_000)
+my_list = list(range(1_000_000))
+my_arr2 = my_arr * 2
+
+data = np.array([[1.5,-0.1,3],[0,-3,6.5]])
+
+print(data)
+#print(data*10)
+#print(data+data)
+
+
+#data2 = [[1,2,3,4],[5,6,7,8]]
+
+#arr2 = np.array(data2)
+#print(arr2.shape)#
+
+
